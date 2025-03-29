62950363927188ccfa70ae51cf25adf5a15a0179
Vishal Kumar Jangid <imrvishal28@gmail.com>
2025-03-29 19:38:34 +0530

    add react-use for debounced search functionality and update movie fetching logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useDebounce } from "react-use";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
@@ -21,13 +22,16 @@ function App() {
 	const [errorMessage, setErrorMessage] = useState('')
 	const [movieList, setMovieList] = useState([])
 	const [isLoading, setIsLoading] = useState(false)
+	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
-	const fetchMovies = async () => {
+	useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+	const fetchMovies = async (query = '') => {
 		setIsLoading(true)
 		setErrorMessage('')
 
 		try {
-			const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+			const endpoint = query ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
 
 			const response = await fetch(endpoint, API_OPTIONS)
 
@@ -53,8 +57,8 @@ function App() {
 	}
 
 	useEffect(() => {
-		fetchMovies()
-	}, [])
+		fetchMovies(debouncedSearchTerm)
+	}, [debouncedSearchTerm])
 
 	return (
 		<main>
