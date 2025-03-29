4553ffb66ab3c4abf8cc86a9b9b7890b9d2d428e
Vishal Kumar Jangid <imrvishal28@gmail.com>
2025-03-29 19:17:02 +0530

    add MovieCard component to display movie details in a structured format

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Search from "./components/Search";
 import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -76,7 +77,7 @@ function App() {
 					) : (
 						<ul>
 							{movieList.map((movie) => (
-								<p key={movie.id} className="text-white">{movie.title}</p>
+								<MovieCard key={movie.id} movie={movie} />
 							))}
 						</ul>
 					)}
